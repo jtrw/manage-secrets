93dc2f4fb838b91aec7b5546ffc4c57b6407fc5c
Nil Borodulia <nborodulia@gmail.com>
2022-02-23 10:30:16 +0300

    change structure

--- backend/app/main.go
+++ backend/app/main.go
@@ -5,18 +5,21 @@ import (
  // "time"
   //"github.com/nilBora/bolt"
   //"store/store"
-  jbolt "manager-secrets/backend/app/store"
+  //jbolt "manager-secrets/backend/app/store"
   secret "manager-secrets/backend/app/store"
 )
 const b = "test/secret"
 
 func main() {
-  db := jbolt.Open("my.db")
-  defer db.Close()
-
-  bolt.Set(db, b, "secondSecret", "888")
-  v := jbolt.Get(db, b, "secondSecret")
-  fmt.Printf("Secret: %s\n", v)
+//   db := jbolt.Open("my.db")
+//   defer db.Close()
+//
+//   bolt.Set(db, b, "secondSecret", "888")
+//   v := jbolt.Get(db, b, "secondSecret")
+//   fmt.Printf("Secret: %s\n", v)
 
   secret.Init();
+  secret.Set(b, "secondSecret", "888")
+  v := secret.Get(b, "secondSecret")
+  fmt.Printf("Secret: %s\n", v)
 }
--- backend/app/store/secret.go
+++ backend/app/store/secret.go
@@ -1,17 +1,22 @@
 package store
 
 import (
-  "fmt"
-  jbolt "bolt"
+  //"fmt"
   "github.com/nilBora/bolt"
+  jbolt "manager-secrets/backend/app/store/jbolt"
 )
 
 var db *bolt.DB;
 
 func Init() {
-  fmt.Printf("Secret!!!")
+  db = jbolt.Open("my.db")
+  //defer db.Close()
 }
 
-func Get(db *bolt.DB, bucket, key string) string {
-    bolt.Get(db, b, "secondSecret")
+func Get(bucket, key string) string {
+    return jbolt.Get(db, bucket, key)
+}
+
+func Set(bucket, key, value string) {
+    jbolt.Set(db, bucket, key, value)
 }
