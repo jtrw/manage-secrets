f52d21de64d95104f2688f343983145e5b801d55
Nil Borodulia <nborodulia@gmail.com>
2022-02-23 19:52:38 +0300

    add go-flags

--- backend/app/main.go
+++ backend/app/main.go
@@ -1,15 +1,38 @@
 package main
 import (
   "fmt"
- // "log"
+  "log"
+  "os"
  // "time"
+  //"flag"
   secret "manager-secrets/backend/app/store"
+  "github.com/jessevdk/go-flags"
 )
 const b = "test/secret"
 
-func main() {
-  secret.Init();
-  secret.Set(b, "secondSecret", "888")
-  v := secret.Get(b, "secondSecret")
-  fmt.Printf("Secret: %s\n", v)
+type Options struct {
+   Name string `long:"name" description:"Your name, for a greeting" default:"Unknown"`
+   Verbose string `short:"v" long:"verbose" description:"Show verbose debug information"`
 }
+
+func main() {
+
+    myProgramName := os.Args[1]
+
+    fmt.Println(myProgramName)
+
+    var opts Options
+    parser := flags.NewParser(&opts, flags.Default)
+    _, err := parser.Parse()
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Name", opts.Name)
+    fmt.Println("Verbose: ", opts.Verbose)
+
+    secret.Init();
+    secret.Set(b, "secondSecret", "888")
+    v := secret.Get(b, "secondSecret")
+    fmt.Printf("Secret: %s\n", v)
+}
+
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require github.com/nilBora/bolt v1.3.1
 
 require (
 	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 )
